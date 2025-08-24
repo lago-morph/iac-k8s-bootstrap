cd69c31b240c9214ea78db3a838119267d942eaa
${GITNAME} <${GITEMAIL}>
2025-08-24 04:57:42 +0000

    use correct tag

--- docker/start_dev.sh
+++ docker/start_dev.sh
@@ -5,7 +5,7 @@ HOMEDIR=/home/ubuntu
 SSH_KEY=${HOMEDIR}/.ssh/${SSH_KEY_FILE}
 SSH_PUBLIC_KEY=${HOMEDIR}/.ssh/${SSH_KEY_FILE}.pub
 
-docker run -it --name dev01 \
+docker run -it --rm --name dev01 \
     -e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" \
     -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
     -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
@@ -14,5 +14,5 @@ docker run -it --name dev01 \
     -e GITNAME="${GITNAME}" \
     -e GITEMAIL="${GITEMAIL}" \
     --volume ${SSH_KEY_DIR}:${HOMEDIR}/.ssh \
-    testimage 
+    devcontainer
     
