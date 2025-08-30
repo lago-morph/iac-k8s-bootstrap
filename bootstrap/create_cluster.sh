2f40244daebee1c45a45e42ff78adc0d43024699
${GITNAME} <${GITEMAIL}>
2025-08-30 23:39:23 +0000

    sequencing node and addon installation

--- bootstrap/create_cluster.sh
+++ bootstrap/create_cluster.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 eksctl create cluster -f cluster.yaml && \
-eksctl create addon -f addon.yaml && \
-eksctl create nodegroup -f nodegroup.yaml
+eksctl create nodegroup -f nodegroup.yaml && \
+eksctl create addon -f pre_addon.yaml && \
+eksctl create addon -f addon.yaml 
