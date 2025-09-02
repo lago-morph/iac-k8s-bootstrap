026618b9ea2495352c61a301e6c1dbb6a1c1f28a
${GITNAME} <${GITEMAIL}>
2025-09-02 04:25:59 +0000

    argocd bootstrap

--- argocd/install.sh
+++ argocd/install.sh
@@ -4,16 +4,20 @@ set -e
 
 helm repo add argocd https://argoproj.github.io/argo-helm
 helm repo update argocd 
-kubectl create ns argocd
+kubectl create ns argocd || /bin/true
 helm install \
 	-f values.yaml \
 	--set global.domain=argocd.$(cat ~/DNS_NAME) \
 	argocd \
 	argocd/argo-cd \
 	-n argocd \
-	--version 8.3.3
+	--version 8.3.3 && \
+kubectl apply -f config -R
 
 export ARGOCD_SERVER=argocd.$(cat ~/DNS_NAME)
+echo "waiting for initial admin secret to be created..."
+kubectl wait --for=create secret/argocd-initial-admin-secret -n argocd --timeout=30s
+echo
 export ARGOCD_PASSWORD=$(kubectl get secret \
     -n argocd \
     argocd-initial-admin-secret \
